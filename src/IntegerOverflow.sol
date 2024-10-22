118ddc99dd4ce498b2c5392506bf76129e43336b
Aidan Kaiser <apkaiser@wisc.edu>
2024-10-22 14:53:37 -0700

    Compilation fix

--- src/IntegerOverflow.sol
+++ src/IntegerOverflow.sol
@@ -1,14 +1,18 @@
 pragma solidity ^0.7.0;
 
 contract Overflow {
-    uint private sellerBalance=0;
+    uint private sellerBalance = 0;
     
-    function add(uint value) returns (bool){
+    // Function that allows overflow
+    function add(uint value) public returns (bool) {
         sellerBalance += value;
+        return true;
     } 
 
-    function safe_add(uint value) returns (bool){
-        require(value + sellerBalance >= sellerBalance);
-        sellerBalance += value; 
+    // Function that prevents overflow using a require check
+    function safe_add(uint value) public returns (bool) {
+        require(value + sellerBalance >= sellerBalance, "Integer overflow");
+        sellerBalance += value;
+        return true;
     } 
 }
